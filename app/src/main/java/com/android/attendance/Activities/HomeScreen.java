e69cedb73c7c715b27d569ae15d024add68f8338
Riyaaz <rahmatullaha845@gmail.com>
2022-05-20 20:13:14 +0530

    Attendance-0.0.2

--- app/src/main/java/com/android/attendance/Activities/HomeScreen.java
+++ app/src/main/java/com/android/attendance/Activities/HomeScreen.java
@@ -1,18 +1,50 @@
 package com.android.attendance.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.android.attendance.databinding.ActivityHomeScreenBinding;
+import com.android.attendance.databinding.*;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
     ActivityHomeScreenBinding binding;
+    AdView mAdView;
+    AdRequest adRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityHomeScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        List<String> testDeviceIds = Arrays.asList("F73AA043D55691CAD3BB9674C77F3D48");
+        RequestConfiguration configuration =
+                new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+        MobileAds.setRequestConfiguration(configuration);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = binding.adView;
+        adRequest = new AdRequest.Builder().build();
+    }
+
+    @Override
+    public void onBackPressed() {
+        for(int i=0;i<=250;i++) {
+            mAdView.loadAd(adRequest);
+        }
     }
 
 }
